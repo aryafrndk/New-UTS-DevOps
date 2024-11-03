59988b0bea32744e72e88c6bcc7ce4f6f5b36169
Arya Frandika Daulay <66843350+aryafrndk@users.noreply.github.com>
2024-11-03 15:28:28 +0700

    Update DAOData.java

--- src/main/java/DAO/DAOData.java
+++ src/main/java/DAO/DAOData.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import model.TambahData;
 import koneksi.DBConnection;
+import java.sql.DriverManager;
 
 public class DAOData implements IDAOData {
 
