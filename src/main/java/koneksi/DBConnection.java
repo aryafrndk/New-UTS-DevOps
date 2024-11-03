a6626c79ba931cfaaccaf6ca3070232b1f068bcb
Arya Frandika Daulay <66843350+aryafrndk@users.noreply.github.com>
2024-11-03 20:03:53 +0700

    Update DBConnection.java

--- src/main/java/koneksi/DBConnection.java
+++ src/main/java/koneksi/DBConnection.java
@@ -1,22 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package koneksi;
+import java.sql.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class DBConnection {
-    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/db_mahasiswa"; // Pastikan URL ini benar
-    static final String USER = "root"; // Username sesuai
-    static final String PASS = ""; // Password kosong
+       
+       static final String DB_URL = "jdbc:mysql://localhost:3306/db_mahasiswa";
+       static final String USER = "root";
+       static final String PASS = "";
+       public static Connection connectDB(){
+           Connection conn = null;
+           try{
+ //              Class.forName("com.mysql.jdbc.Driver");
+               conn = DriverManager.getConnection(DB_URL,USER,PASS);
+               return conn;
+           }catch(SQLException ex){
+               System.out.println("There were erorrs while connecting to db.");
+               return null;
+           }
+       }
 
-    public static Connection connectDB() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            return conn;
-        } catch (SQLException ex) {
-            System.out.println("There were errors while connecting to db: " + ex.getMessage());
-            return null;
-        }
-    }
+    
 }
